4e5d43a740b661f950e879a89fa76780d272b0cf
H.M.Zahid <bcsf15m019@pucit.edu.pk>
2018-05-09 16:24:51 +0000

    Updated

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-obj-m+=hello.o
+
+obj-m=hello.o
+
+KVERSION=$(shell uname -r)
 
 all:
-	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
-clean:
-	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
+	make -C /lib/modules/$(KVERSION)/build M=$(PWD) modules
 
+clean:
+	make -C /lib/modules/$(KVERSION)/build M=$(PWD) clean
--- hello.c
+++ hello.c
@@ -1,28 +1,18 @@
 #include<linux/init.h>
 #include<linux/module.h>
-#include<linux/kernel.h>
 
-MODULE_LICENCE("GPL");
-MODULE_AUTHOR("Zahid");
-MODULE_DESCRIPTION("It is simple hello world LKM");
-MODULE_VERSION("1.0");
-
-static char *name="ZAHID";
-module_param(name,charp,S_IRUGO);
-MODULE_PARM_DESC(name,"The name is to display in /var/log/kern.log");
-
-static int __init hello_init(void)
+static int hello_init(void)
 {
-	printk(KERN_INFO,"HELOO %s From LKM\n",name);
-	return 0;
+	printk(KERN_ALERT "Hello World\n");
+return 0;
 }
 
-
-static void __exit hello_exit(void)
+static void hello_exit(void)
 {
-	printk(KERN_INFO,"Bye %s From LKM\n");
+	printk(KERN_ALERT "Good Bye\n");
 }
-
 
 module_init(hello_init);
 module_exit(hello_exit);
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("ZAHID");

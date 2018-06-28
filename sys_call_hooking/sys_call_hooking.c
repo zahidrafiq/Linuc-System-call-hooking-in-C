88088d5ecca5bb4bbdc20340373b6f7d4281cb15
H.M.Zahid <bcsf15m019@pucit.edu.pk>
2018-06-28 22:07:14 +0000

    sys_call_hooking updated c8

--- sys_call_hooking/sys_call_hooking.c
+++ sys_call_hooking/sys_call_hooking.c
@@ -1,18 +1,21 @@
-#include<linux/init.h>
-#include<linux/module.h>
+#include <linux/init.h>
 #include <linux/kernel.h>
+#include <linux/module.h>
 #include <linux/syscalls.h>
 
-int counter = 0;
+static int counter = 0;
 
 unsigned long **sys_call_table;
 
-asmlinkage long (*ref_sys_write)(unsigned int fd, const char __user *buf, size_t count);
+//asmlinkage long (*ref_sys_read)(unsigned int fd, const char __user *buf, size_t count);
 
-//Our new write system call that will be called inspite of origional write system call
-asmlinkage long new_sys_write(unsigned int fd, const char __user *buf, size_t count)
-{   
-    return ref_sys_write(fd,buf ,count);
+asmlinkage int (*ref_sys_open) (const char*, int, int);
+
+// Our fake system call that calls the original system call
+asmlinkage long new_sys_open(const char* file, int flags, int mode)
+{	
+	printk(KERN_INFO "Your System call is hooked : %s",file);
+        return ref_sys_open(file, flags, mode);
 }
 
 // this function returns the pointer of the system call table
@@ -32,14 +35,11 @@ static unsigned long **aquire_sys_call_table(void)
     return NULL;
 }
 
-
-// This function disable the memory protection
+// This function disable the memory protection.
 static void disable_page_protection(void) 
 {
-	printk(KERN_ALERT "Disabling page protection\n");
     unsigned long value;
     asm volatile("mov %%cr0, %0" : "=r" (value));
-
     if(!(value & 0x00010000))
         return;
 
@@ -49,7 +49,6 @@ static void disable_page_protection(void)
 // This function enable the memory protection
 static void enable_page_protection(void) 
 {
-	printk(KERN_ALERT "Enabling Page protection\n");
     unsigned long value;
     asm volatile("mov %%cr0, %0" : "=r" (value));
 
@@ -59,19 +58,18 @@ static void enable_page_protection(void)
     asm volatile("mov %0, %%cr0" : : "r" (value | 0x00010000));
 }
 
-
 // This function is executed when the module is loaded.
-static int __init prog_start(void) 
+static int __init hooking_start(void) 
 {
-	printk(KERN_ALERT "Module is loaded\n");
+    printk(KERN_INFO "init");
     if(!(sys_call_table = aquire_sys_call_table())) {
              return -1;
     }
 
     disable_page_protection();
 
-    ref_sys_write = (void *)sys_call_table[__NR_write];
-    sys_call_table[__NR_write] = (unsigned long *)new_sys_write;
+    ref_sys_open = (void *)sys_call_table[__NR_open];
+    sys_call_table[__NR_open] = (unsigned long *)new_sys_open;
 
     enable_page_protection();
 
@@ -79,16 +77,15 @@ static int __init prog_start(void)
 }
 
 // This method is executed when the module is removed/unloaded
-static void __exit prog_exit(void) 
+static void __exit hooking_end(void) 
 {
-	printk(KERN_ALERT "Module is unloading\n");
-
+	printk(KERN_INFO "exit");
     if(!sys_call_table)
-        return; 
-    sys_call_table[__NR_write] = (unsigned long *)ref_sys_write;
-   
+        return;
+    disable_page_protection();
+	 sys_call_table[__NR_open] = (unsigned long *)ref_sys_open;
+    enable_page_protection();
 }
 
-module_init(prog_start);
-module_exit(prog_exit);
-
+module_init(hooking_start);
+module_exit(hooking_end);
--- system call hooking/.tmp_versions/write_sys_call.mod
+++
@@ -1,3 +0,0 @@
-/home/zahid/SP/project/pp/system-call-hooking-linux/write_sys_call_hooking/write_sys_call.ko
-/home/zahid/SP/project/pp/system-call-hooking-linux/write_sys_call_hooking/write_sys_call.o
-
